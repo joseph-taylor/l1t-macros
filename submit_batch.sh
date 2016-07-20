a90ea92b7c6aabfaddd18341580e56fd79d57241
joseph taylor <joseph.ross.taylor@cern.ch>
2016-07-20 15:12:11 +0200

    to submit lxplus batch jobs

---
+++ submit_batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run with $ bsub -q 8nh "sh submit_batch.sh <options>"
+cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_9/src/
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/user/t/taylor/l1t-macros/
+
+root -q -b -l makeJetResolutions.cxx
+# root -q -b -l makeJetTurnons.cxx
+# root -q -b -l makeJetXvsY.cxx
+# root -q -b -l makeResolutions.cxx
+# root -q -b -l makeTurnons.cxx
+# root -q -b -l makeXvsY.cxx
