49fc43c7b342cbfc4841acb56cf7f08cac2c0a54
joseph taylor <joseph.ross.taylor@cern.ch>
2016-07-20 16:02:04 +0200

    lxbatch run setup...removed *.root from Directory name

--- submit_batch.sh
+++ submit_batch.sh
@@ -4,9 +4,9 @@ cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_9/src/
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/user/t/taylor/l1t-macros/
 
-root -q -b -l makeJetResolutions.cxx
+# root -q -b -l makeJetResolutions.cxx
 # root -q -b -l makeJetTurnons.cxx
 # root -q -b -l makeJetXvsY.cxx
 # root -q -b -l makeResolutions.cxx
 # root -q -b -l makeTurnons.cxx
-# root -q -b -l makeXvsY.cxx
+root -q -b -l makeXvsY.cxx
