cfcae58a44755840aaaaef45739a4358de874516
joseph taylor <joseph.ross.taylor@cern.ch>
2016-07-22 18:10:54 +0200

    new batch submissions script so no mixing

---
+++ submit_makeTurnons.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# run with $ bsub -q 8nh "sh submit_batch.sh <options>"
+# $ bsub -q 8nh "sh submit_makeTurnons.sh"
+cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_9/src/
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/user/t/taylor/l1t-macros/
+
+root -q -b -l makeTurnons.cxx
