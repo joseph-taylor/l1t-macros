13b5df11c59d31ae0342199a3320a76f05c7a6f9
joseph taylor <joseph.ross.taylor@cern.ch>
2016-07-24 01:08:10 +0200

    deleted old files

--- submit_makeResolutions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# run with $ bsub -q 8nh "sh submit_batch.sh <options>"
-# $ bsub -q 8nh "sh submit_makeResolutions.sh"
-cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_9/src/
-eval `scramv1 runtime -sh`
-cd /afs/cern.ch/user/t/taylor/l1t-macros/
-
-root -q -b -l makeResolutions.cxx
