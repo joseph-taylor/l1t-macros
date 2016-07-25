2c2c7c6459d6e14b30c9230552cf33ef701e1e2f
joseph taylor <joseph.ross.taylor@cern.ch>
2016-07-25 12:21:41 +0200

    small fix to the outputting

--- python/haddRootFiles.py
+++ python/haddRootFiles.py
@@ -6,7 +6,8 @@ from runList import listOfRunsForPython
 # what about .root files which are ~null???
 ######################################################################
 ######################################################################
-motherDirectory = "parallelRunning_TESTINGv3/"
+motherDirectory = "parallelRunning_ICHEPv2redoRESO/"
+dirPath = "/afs/cern.ch/user/t/taylor/l1t-macros/output_plots/" + motherDirectory
 
 resJets = ["res_SingleMu_jetEt_over_l1JetEt_barrel-endcap.root",
            "res_SingleMu_jetEta_over_l1JetEta.root",
@@ -31,21 +32,20 @@ xyJets = ["xy_SingleMu_jetEt_vs_l1JetEt_barrel-endcap.root",
 ######################################################################
 ######################################################################
 
-os.system("cd /afs/cern.ch/user/t/taylor/l1t-macros/output_plots/%s" % motherDirectory)
-os.system("mkdir combinedRuns/")
-os.system("mkdir combinedRuns/resJets/")
-os.system("mkdir combinedRuns/Resolutions/")
-os.system("mkdir combinedRuns/Turnons/")
-os.system("mkdir combinedRuns/TurnonsJets/")
-os.system("mkdir combinedRuns/xy/")
-os.system("mkdir combinedRuns/xyJets/")
+os.system("mkdir %scombinedRuns/" % dirPath)
+os.system("mkdir %scombinedRuns/resJets/" % dirPath)
+os.system("mkdir %scombinedRuns/Resolutions/" % dirPath)
+os.system("mkdir %scombinedRuns/Turnons/" % dirPath)
+os.system("mkdir %scombinedRuns/TurnonsJets/" % dirPath)
+os.system("mkdir %scombinedRuns/xy/" % dirPath)
+os.system("mkdir %scombinedRuns/xyJets/" % dirPath)
 
 i = 0
 for rootFile in resJets:
 
-	commandString = "hadd -k combinedRuns/resJets/" + rootFile + " "
+	commandString = "hadd -k " + dirPath + "combinedRuns/resJets/" + rootFile + " "
 	for runNumber in listOfRunsForPython:
-		commandString = commandString + "resJets/run" + runNumber +  "/" + rootFile + " "
+		commandString = commandString + dirPath + "resJets/run" + runNumber +  "/" + rootFile + " "
 
 	# print commandString
 	os.system("%s" % commandString)
@@ -58,9 +58,9 @@ print("Done resJets:)\n\n")
 i = 0
 for rootFile in Resolutions:
 
-	commandString = "hadd -k combinedRuns/Resolutions/" + rootFile + " "
+	commandString = "hadd -k " + dirPath + "combinedRuns/Resolutions/" + rootFile + " "
 	for runNumber in listOfRunsForPython:
-		commandString = commandString + "Resolutions/run" + runNumber +  "/" + rootFile + " "
+		commandString = commandString + dirPath + "Resolutions/run" + runNumber +  "/" + rootFile + " "
 
 	# print commandString
 	os.system("%s" % commandString)
@@ -73,9 +73,9 @@ print("Done Resolutions:)\n\n")
 i = 0
 for rootFile in Turnons:
 
-	commandString = "hadd -k combinedRuns/Turnons/" + rootFile + " "
+	commandString = "hadd -k " + dirPath +"combinedRuns/Turnons/" + rootFile + " "
 	for runNumber in listOfRunsForPython:
-		commandString = commandString + "Turnons/run" + runNumber +  "/" + rootFile + " "
+		commandString = commandString + dirPath + "Turnons/run" + runNumber +  "/" + rootFile + " "
 
 	# print commandString
 	os.system("%s" % commandString)
@@ -88,9 +88,9 @@ print("Done Turnons:)\n\n")
 i = 0
 for rootFile in TurnonsJets:
 
-	commandString = "hadd -k combinedRuns/TurnonsJets/" + rootFile + " "
+	commandString = "hadd -k " + dirPath + "combinedRuns/TurnonsJets/" + rootFile + " "
 	for runNumber in listOfRunsForPython:
-		commandString = commandString + "TurnonsJets/run" + runNumber +  "/" + rootFile + " "
+		commandString = commandString + dirPath + "TurnonsJets/run" + runNumber +  "/" + rootFile + " "
 
 	# print commandString
 	os.system("%s" % commandString)
@@ -103,9 +103,9 @@ print("Done TurnonsJets:)\n\n")
 i = 0
 for rootFile in xy:
 
-	commandString = "hadd -k combinedRuns/xy/" + rootFile + " "
+	commandString = "hadd -k " + dirPath + "combinedRuns/xy/" + rootFile + " "
 	for runNumber in listOfRunsForPython:
-		commandString = commandString + "xy/run" + runNumber +  "/" + rootFile + " "
+		commandString = commandString + dirPath + "xy/run" + runNumber +  "/" + rootFile + " "
 
 	# print commandString
 	os.system("%s" % commandString)
@@ -118,9 +118,9 @@ print("Done xy:)\n\n")
 i = 0
 for rootFile in xyJets:
 
-	commandString = "hadd -k combinedRuns/xyJets/" + rootFile + " "
+	commandString = "hadd -k " + dirPath + "combinedRuns/xyJets/" + rootFile + " "
 	for runNumber in listOfRunsForPython:
-		commandString = commandString + "xyJets/run" + runNumber +  "/" + rootFile + " "
+		commandString = commandString + dirPath + "xyJets/run" + runNumber +  "/" + rootFile + " "
 
 	# print commandString
 	os.system("%s" % commandString)
