5b3a61bff7f0b115832e1ed6160e10559efb75ec
joseph taylor <joseph.ross.taylor@cern.ch>
2016-09-26 17:08:42 +0200

    hack to use emulator rather than data

--- Event/TL1PrimitiveEventClass.h
+++ Event/TL1PrimitiveEventClass.h
@@ -61,7 +61,8 @@ TL1PrimitiveEventClass::TL1PrimitiveEventClass(std::vector<std::string> inDir) :
     metFilterReco(new TL1DataClass("l1MetFilterRecoTree/MetFilterRecoTree",inDir)),
     muonReco(new TL1DataClass("l1MuonRecoTree/Muon2RecoTree",inDir)),
     recoTree(new TL1DataClass("l1RecoTree/RecoTree",inDir)),
-    upgrade(new TL1DataClass("l1UpgradeTree/L1UpgradeTree",inDir)),
+    // upgrade(new TL1DataClass("l1UpgradeTree/L1UpgradeTree",inDir)),
+    upgrade(new TL1DataClass("l1UpgradeEmuTree/L1UpgradeTree",inDir)), // THIS HACKS MAKES IT WORK FOR THE EMULATOR
     fCaloTowers(new L1Analysis::L1AnalysisL1CaloTowerDataFormat()),
     fJets      (new L1Analysis::L1AnalysisRecoJetDataFormat()),
     fSums      (new L1Analysis::L1AnalysisRecoMetDataFormat()),
