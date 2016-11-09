afad19a9cc05eb163dfecc9eff80ae2bd560690d
joseph taylor <joseph.ross.taylor@cern.ch>
2016-11-09 10:32:33 +0100

    updated submission to use new cmssw

--- shellSubmission/submit_makeXvsY.sh
+++ shellSubmission/submit_makeXvsY.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # run with $ bsub -q 8nh "sh submit_makeXvsY.sh <options in space seperated list>"
 # $ bsub -q 8nh "sh submit_makeXvsY.sh"
-cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_9/src/
+# cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_9/src/
+cd /afs/cern.ch/user/t/taylor/CMSSW_8_0_21/src/
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/user/t/taylor/l1t-macros/
 
